813586c98ffb230f265d2f04843b9dbb5a806875
Hash <takuya21hashimoto@gmail.com>
2013-03-17 18:46:57 +0900

    use Forwardable standard lib to delegate methods

--- ruby-design-pattern/decorator.rb
+++ ruby-design-pattern/decorator.rb
@@ -1,4 +1,6 @@
 # encoding: utf-8
+require 'forwardable'
+
 class SimpleWriter
   def initialize(path)
     @file = File.open(path, 'w')
@@ -23,24 +25,12 @@ class SimpleWriter
 end
 
 class WriteDecorator
+  extend Forwardable
+
+  def_delegators :@real_writer, :write_line, :pos, :rewind, :close
+
   def initialize(real_writer)
     @real_writer = real_writer
-  end
-
-  def write_line(line)
-    @real_writer.write_line(line)
-  end
-
-  def pos
-    @real_writer.pos
-  end
-
-  def rewind
-    @real_writer.rewind
-  end
-
-  def close
-    @real_writer.close
   end
 end
 
