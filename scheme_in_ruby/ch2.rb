f394fe09404c821f37fe150e1973d8ea1154129c
Hash <takuya21hashimoto@gmail.com>
2013-05-30 02:13:38 +0900

    add memo

--- scheme_in_ruby/ch2.rb
+++ scheme_in_ruby/ch2.rb
@@ -5,3 +5,10 @@ exp = [[:lambda, [:x, :y], [:+, :x, :y]], 3, 2]
 # puts $global_env
 # {:+=>[:prim, #<Proc:0x007fb53c8255e8@/Users/hash/work/study/scheme_in_ruby/eval.rb:30 (lambda)>], :-=>[:prim, #<Proc:0x007fb53c825598@/Users/hash/work/study/scheme_in_ruby/eval.rb:31 (lambda)>], :*=>[:prim, #<Proc:0x007fb53c825548@/Users/hash/work/study/scheme_in_ruby/eval.rb:32 (lambda)>]}
 puts _eval(exp, $global_env)
+
+
+# λ式 => 単なるコード
+# クロージャ => λ式と, それを評価した時の環境
+
+# λ式が扱える = 関数を値として扱える = 高階関数がある
+# クロージャは高階関数よりもさらに強力
