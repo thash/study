17de6c7d96611b28bd851c04a1a3bb1f4b5337d0
Hash <takuya21hashimoto@gmail.com>
2013-06-05 03:14:13 +0900

    add repl

--- scheme_in_ruby/eval.rb
+++ scheme_in_ruby/eval.rb
@@ -340,3 +340,56 @@ end
 def quote?(exp)
   exp[0] == :quote
 end
+
+### 4.6. REPL
+def repl
+  prompt = '>>> '
+  second_prompt = '> '
+  while true
+    print prompt
+    line = gets or return
+    while line.count('(') > line.count(')')
+      print second_prompt
+      next_line = gets or return
+      line += next_line
+    end
+    redo if line =~ /\A\s*\z/m
+    begin
+      val = _eval(parse(line), $global_env)
+    rescue Exception => e
+      puts e.to_s
+      redo
+    end
+    puts pp(val)
+  end
+end
+
+def closure?(exp)
+  exp[0] == :closure
+end
+
+def pp(exp)
+  if exp.is_a?(Symbol) or num?(exp)
+    exp.to_s
+  elsif exp == nil
+    'nil'
+  elsif exp.is_a?(Array) and closure?(exp)
+    parameter, body, env = exp[1], exp[2], exp[3]
+    "(closure #{pp(parameter)} #{pp(body)})"
+  elsif exp.is_a?(Hash)
+    if exp == $primitive_fun_env
+      '*primitive_fun_env*'
+    elsif exp == $boolean_env
+      '*boolean_env*'
+    elsif exp == $list_env
+      '*list_env*'
+    else
+      '{' + exp.map{|k, v| pp(k) + ':' + pp(v)}.join(', ') + '}'
+    end
+  elsif exp.is_a?(Array)
+    '(' + exp.map{|e| pp(e)}.join(', ') + ')'
+  else
+    exp.to_s
+  end
+end
+
