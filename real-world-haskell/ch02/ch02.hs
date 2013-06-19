92f2971fd2c68e7e1d715e3da0527e520167cb67
Hash <takuya21hashimoto@gmail.com>
2013-06-20 02:07:56 +0900

    study 2013-06-19

--- real-world-haskell/ch02/ch02.hs
+++ real-world-haskell/ch02/ch02.hs
@@ -35,11 +35,60 @@ it :: Int
 -- Prelude> compare 2 3 == LT
 -- True
 
--- Prelude> :type [(True, []), (False, [['a']])]
--- [(True, []), (False, [['a']])] :: [(Bool, [[Char]])]
+Prelude> :type [(True, []), (False, [['a']])]
+[(True, []), (False, [['a']])] :: [(Bool, [[Char]])]
 
 -- 空リストを持つリストは空リストそのものとは別物.
--- Prelude> [[]] /= []
--- True
+Prelude> [[]] /= []
+True
 
+
+Prelude> fst (1,'a')
+1
+Prelude> fst (1,'a',"lslsls")
+<interactive>:5:5:
+    Couldn't match expected type `(a0, b0)'
+                with actual type `(t0, Char, [Char])'
+    In the first argument of `fst', namely `(1, 'a', "lslsls")'
+    In the expression: fst (1, 'a', "lslsls")
+    In an equation for `it': it = fst (1, 'a', "lslsls")
+Prelude> :type fst
+fst :: (a, b) -> a
+Prelude> :type snd
+snd :: (a, b) -> b
+
+
+-- let XXX in YYY
+-- XXXで定義したものはYYY中でしか使えない
+
+-- 関数適用は左結合
+-- a b c d は (((a b) c) d) と同じ
+
+-- 関数に:typeを作用させた時の->は "to" と読む.
+
+-- 副作用がある関数は型シグネチャにIO Stringが出てくる.
+-- ghciと.hsファイルでの微妙な書き方の違いの根源はIOモナドにある. 7章を待て
+
+Prelude> :type getLine
+getLine :: IO String
+
+Prelude> :load Assign.hs
+--[1 of 1] Compiling Main             ( Assign.hs, interpreted )
+-- Assign.hs:2:1:
+--     Multiple declarations of `x'
+--     Declared at: Assign.hs:1:1
+--                  Assign.hs:2:1
+-- Failed, modules loaded: none.
+
+
+-- in ghci
+Prelude> x = 102
+<interactive>:3:3: parse error on input `='
+
+Prelude> let x = 102
+Prelude> x
+102
+
+-- ghciでは x = 1, とするとエラー. let x = 1, のようにletを付ける必要がある.
+-- ファイルの代入だとエラーなんだけどghciのletだと再束縛ok.
 
