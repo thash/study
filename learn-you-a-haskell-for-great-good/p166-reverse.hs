2c553ad783d43de94bdfdce0632ee47b361da717
memerelics <takuya21hashimoto@gmail.com>
2015-04-06 20:15:10 +0900

    Haskell I/O

--- learn-you-a-haskell-for-great-good/p166-reverse.hs
+++ learn-you-a-haskell-for-great-good/p166-reverse.hs
@@ -1,4 +1,5 @@
 main = do
+    -- さっそく束縛
     line <- getLine
     if null line
         then return ()
@@ -9,3 +10,5 @@ main = do
 reverseWords :: String -> String
 reverseWords = unwords . map reverse . words
 
+-- Haskellのreturnは実行を終了しない．returnは結果を生成するIOアクションを作り出し，結果を捨てる．
+-- 捨てたくなければ a <- return "hi" とする
--- learn-you-a-haskell-for-great-good/sec8-io.hs
+++ learn-you-a-haskell-for-great-good/sec8-io.hs
@@ -1,14 +1,22 @@
 -- 人間からコンピュータに与えるのは、一連の実行ステップではなく、あるものが何であるかの定義です。
 
+-- () は unit. putStrLnは意味のある返り値を持たないのでダミーとして．
+-- Prelude> :t putStrLn
+-- putStrLn :: String -> IO ()
+
+-- doのあとにずらずらIOアクションを並べると，それがひとつのIOアクションに"のりづけ"される
+-- doを書いた後はあたかも命令型プログラムのように実行ステップを書き並べる
 main = do
     putStrLn "Hello, what's your name?"
-    name <- getLine
+    name <- getLine -- Stringを生成するIOアクション
     putStrLn ("Hey " ++ name ++ ", you rock!")
 
 -- *Main> :t getLine
 -- getLine :: IO String
 -- putStrLnもgetLineも、IOアクションである。それをdoを使って糊付けすればひとつのI/Oアクションになる。
 
+-- "hoge" ++ getLine とか， var = getLine
+-- みたいな書き方はできない．どこかで<-を使って束縛するしかない
 
 import Data.Char
 
@@ -17,6 +25,7 @@ main = do
     firstName <- getLine
     putStrLn "What's your last name?"
     lastName <- getLine
+    -- letは<-と違って「普通の(純粋な)値にに名前を与える」
     let bigFirstName = map toUpper firstName
         bigLastName = map toUpper lastName
     putStrLn $ "hey" ++ bigFirstName ++ " "
